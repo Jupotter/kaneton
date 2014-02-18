997ffb18a6bce693585ace97b3eb70d392da642f
Jupotter <jupotter.g@gmail.com>
2014-02-18 17:54:56 +0100

    Initialize repository

---
+++ environment/profile/user/garagn_j/setup_env.sh
@@ -0,0 +1,6 @@
+export KANETON_USER="garagn_j"
+export KANETON_HOST="linux/ia32"
+export KANETON_PLATFORM="ibm-pc"
+export KANETON_ARCHITECTURE="ia32/educational"
+export KANETON_PYTHON="/usr/bin/python2"
+
---
+++ environment/profile/user/group24/setup_env.sh
@@ -0,0 +1,6 @@
+export KANETON_USER="group24"
+export KANETON_HOST="linux/ia32"
+export KANETON_PLATFORM="ibm-pc"
+export KANETON_ARCHITECTURE="ia32/educational"
+export KANETON_PYTHON="/usr/bin/python2"
+
