f92adfb57773e25a587772c8657d07a1f2488d35
Laport Mérédith <laport_a@epita.fr>
2014-02-20 15:24:07 +0100

    [K1] Beginning machine/glue/event.c tab of function for grub event dispatcher

--- kaneton/machine/glue/ibm-pc.ia32/educational/event.c
+++ kaneton/machine/glue/ibm-pc.ia32/educational/event.c
@@ -33,15 +33,59 @@
 
 d_event			glue_event_dispatch =
   {
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL
+    glue_event_show,
+    glue_event_dump,
+    glue_event_notify,
+    glue_event_enable,
+    glue_event_disable,
+    glue_event_reserve,
+    glue_event_release,
+    glue_event_initialize,
+    glue_event_clean
   };
 
-/* FIXME[code to complete] */
+/* BEGIN FIXME[code to complete] */
+
+t_status                glue_event_show(i_event                id,
+                                        mt_margin              margin)
+{
+
+}
+
+t_status                glue_event_dump(void)
+{
+}
+
+t_status                glue_event_notify(i_event              id)
+{
+}
+
+t_status                glue_event_enable(void)
+{
+}
+
+t_status                glue_event_disable(void)
+{
+}
+
+t_status                glue_event_reserve(i_event             id,
+                                           t_type              type,
+                                           u_event_handler     handle,
+                                           t_data              data)
+{
+
+}
+
+t_status                glue_event_release(i_event             id)
+{
+}
+
+t_status                glue_event_initialize(void)
+{
+}
+
+t_status                glue_event_clean(void)
+{
+}
+/* END FIXME[code to complete] */
+
