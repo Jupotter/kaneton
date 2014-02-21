1cd5d05bb4ea418bd32a82acaedebac2f44b1c8f
Jupotter <jupotter.g@gmail.com>
2014-02-21 16:59:22 +0100

    Add logging to glue functions

--- kaneton/machine/glue/ibm-pc.ia32/educational/event.c
+++ kaneton/machine/glue/ibm-pc.ia32/educational/event.c
@@ -32,58 +32,86 @@
  */
 
 d_event                 glue_event_dispatch =
-  {
-    NULL,
-    NULL,
+{
+    glue_event_show,
+    glue_event_dump,
     NULL,
     glue_event_enable,
-    NULL,
+    glue_event_disable,
     glue_event_reserve,
     NULL,
     glue_event_initialize,
     NULL
-  };
+};
 
-/* BEGIN FIXME[code to complete] */
-
-#if 0
-t_status                glue_event_show(i_event                id,
-                                        mt_margin              margin)
+t_status glue_event_dump(void)
 {
-    o_event*            o;
-
-    if (event_get(id, &o) != STATUS_OK)
-        MACHINE_ESCAPE("unable to retrieve the event object");
-
-    module_call(console, message,
-                '#',
-                MODULE_CONSOLE_MARGIN_FORMAT
-                " machine:\n",
-                MODULE_CONSOLE_MARGIN_VALUE(margin));
+    architecture_idt_dump();
 
     MACHINE_LEAVE();
 }
-#endif
+
+/* BEGIN FIXME[code to complete] */
+
+t_status                glue_event_show(i_event                id,
+        mt_margin              margin)
+{
+//    o_event*            o;
+
+    //    if (event_get(id, &o) != STATUS_OK)
+    //        MACHINE_ESCAPE("unable to retrieve the event object");
+
+    module_call(console, message,
+            '#',
+            MODULE_CONSOLE_MARGIN_FORMAT
+            " machine:\n",
+            MODULE_CONSOLE_MARGIN_VALUE(margin));
+
+    MACHINE_LEAVE();
+}
 
 t_status                glue_event_enable(void)
 {
+    module_call(console, message,
+            '!', "Enabling events\n");
+
+    MACHINE_LEAVE();
+}
+
+t_status                glue_event_disable(void)
+{
+    module_call(console, message,
+            '!', "Disabling events\n");
+
     MACHINE_LEAVE();
 }
 
 t_status                glue_event_reserve(i_event             id,
-                                           t_type              type,
-                                           u_event_handler     handle,
-                                           t_data              data)
+        t_type              type,
+        u_event_handler     handle,
+        t_data              data)
 {
     o_event*            o;
 
     if (event_get(id, &o) != STATUS_OK)
         MACHINE_ESCAPE("unable to retrieve the event")
 
-    if (architecture_idt_clear(id) != STATUS_OK)
-        MACHINE_ESCAPE("this architecture can't be build");
+            if (architecture_idt_clear(id) != STATUS_OK)
+                MACHINE_ESCAPE("this architecture can't be build");
 
+    module_call(console, message,
+            '!', "Reserving event: %i\n",
+            id);
     //&o->machine.idt.flush = BOOLEAN_FALSE;
+
+    MACHINE_LEAVE();
+}
+
+t_status                glue_event_release(i_event  id)
+{
+    module_call(console, message,
+            '!', "Releasing event: %i\n",
+            id);
 
     MACHINE_LEAVE();
 }
